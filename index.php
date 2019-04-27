d48c316dbb39f0fef297f5270ca2aa86307f39bb
Ricardo Belfort <rbelfort2004@gmail.com>
2019-04-27 18:31:12 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Agência digital focada em Criação de Websites, Marketing Digital e Identidade Visual em manaus no Amazonas." />
+    <meta name="robots" content="index, follow" />
+    
+    <meta itemprop="name" content="Belfort Web | Design & Internet"/>
+    <meta itemprop="description" content="Agência digital focada em Criação de Websites, Marketing Digital e Identidade Visual em manaus no Amazonas."/>
+    <meta itemprop="image" content="http://belfortweb.com.br/themes/belfortweb/images/sitekit.jpg"/>
+    <meta itemprop="url" content="http://belfortweb.com.br"/>
+
+    <link rel="canonical" href="http://belfortweb.com.br"/>
+    <link rel="base" href="http://belfortweb.com.br"/>
+
+    <!-- Facebook Open Graph -->
+    <meta property="og:url" content="http://belfortweb.com.br"/>
+    <meta property="og:title" content="Belfort Web | Design & Internet"/>
+    <meta property="og:description" content="Agência digital focada em Criação de Websites, Marketing Digital e Identidade Visual em manaus no Amazonas."/>
+    <meta property="og:image" content="http://belfortweb.com.br/themes/belfortweb/images/sitekit.jpg"/>
+    <meta property="og:image:type" content="image/png"/>
+    <meta property="og:image:width" content="600"/>
+    <meta property="og:image:height" content="600"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:locale" content="pt_BR"/>
+    <meta property="og:app_id" content="215608452711736"/>
+
+    <!-- Fontawesome + Google Fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet">
+
+    <!-- CSS Styles -->
+    <link rel="stylesheet" href="assets/styles/reset.css">
+    <link rel="stylesheet" href="assets/styles/style.css"/>
+    <link rel="shortcut icon" href="themes/belfortweb/images/favicon.png"/>
+    
+    <title>Belfort Web | Design & Internet em Manaus no Amazonas Brasil</title>
+    <script src="themes/belfortweb/js/modernizr.custom.js"></script>
+        <!-- Start of Async Drift Code -->
+        <script>
+        "use strict";
+
+        !function() {
+        var t = window.driftt = window.drift = window.driftt || [];
+        if (!t.init) {
+            if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+            t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+            t.factory = function(e) {
+            return function() {
+                var n = Array.prototype.slice.call(arguments);
+                return n.unshift(e), t.push(n), t;
+            };
+            }, t.methods.forEach(function(e) {
+            t[e] = t.factory(e);
+            }), t.load = function(t) {
+            var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+            o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+            var i = document.getElementsByTagName("script")[0];
+            i.parentNode.insertBefore(o, i);
+            };
+        }
+        }();
+        drift.SNIPPET_VERSION = '0.3.1';
+        drift.load('krxpyddtvge3');
+    </script>
+    <!-- End of Async Drift Code -->
+</head>
+<body>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126485124-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-126485124-1');
+    </script>
+
+    <header class="main-header">
+        <div class="main-header-content">
+            <div class="main-header-logo">
+                <a href="index.html" title="Belfort Web | Design & Internet em Manaus no Amazonas Brasil">
+                    <img src="themes/belfortweb/images/belfortweb-logo.png" alt="Belfort Web | Design & Internet em Manaus no Amazonas Brasil">
+                    <h1 class="fontzero">Belfort Web | Design & Internet em Manaus no Amazonas Brasil</h1>
+                </a>
+            </div>
+            <div class="main-header-social">
+                <ul>
+                    <li class="title">social media</li>
+                    <li>
+                        <a href="https://www.facebook.com/belfortweb" title="Belfort Web no facebook" target="_blank" rel="nofollow">
+                            <i class="fab fa-facebook-f fa-lg"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/belfortweb/" title="Belfort Web no instagram" target="_blank" rel="nofollow">
+                            <i class="fab fa-instagram fa-lg"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <section class="main-headline">
+                <header class="title">
+                    <h1 class="text-shadow">Belfort Web <br> Agência Digital</h1>
+                    <p class="text-shadow">Uma agência focada em desenvolvimento web, mídias sociais e design gráfico.</p>
+                </header>
+                <div>
+                    <img src="themes/belfortweb/images/agencia-belfortweb.png" title="Agência Belfort Web" alt="Agência Belfort Web">
+                </div>
+            </section>
+        </div>
+    </header>
+
+    <main>
+        <!-- Secão Serviços -->    
+        <section class="service-section">
+            <div class="content">
+                <header class="service-section-title">
+                    <h1>Serviços</h1>
+                    <p>Como posso te ajudar no seu projeto?</p>
+                    <span class="divisor"></span>
+                </header>
+                <div class="service-section-services">
+                    <article class="service-section-services-article">
+                        <img src="themes/belfortweb/images/icons/icon-websites.png" title="Desenvolvimento de Websites" alt="Desenvolvimento de Websites">
+                        <h2>Websites</h2>
+                        <p>Criação de Websites, Blogs e Lojas virtuais personalizados para sua empresa.</p>
+                    </article>
+                    <article class="service-section-services-article">
+                        <img src="themes/belfortweb/images/icons/icon-social-media.png" title="Desenvolvimento de Websites" alt="Desenvolvimento de Websites">
+                        <h2>Mídias Sociais</h2>
+                        <p>Anúncios, Capas, Stories. Campanhas de email marketing, Landing pages e Ads.</p>
+                    </article>
+                    <article class="service-section-services-article">
+                        <img src="themes/belfortweb/images/icons/icon-design-grafico.png" title="Desenvolvimento de Websites" alt="Desenvolvimento de Websites">
+                        <h2>Design Gráfico</h2>
+                        <p>Identidade Visual, Manual de uso, Redesign de marca e Projetos gráficos.</p>
+                    </article>
+                </div>
+            </div>
+        </section>
+
+        <!-- Secão Portfólio -->
+        <section class="portfolio-section">
+            <div class="content">
+                <header class="portfolio-section-title">
+                    <h1>Portfólio</h1>
+                    <p>Conheça um pouco do meu trabalho</p>
+                    <span class="divisor"></span>
+                </header>
+                <div class="portfolio-section-jobs">
+
+                <article class="portfolio-section-jobs-article">
+                    <h1 class="fontzero">Trabalhos</h1>
+                    <ul class="grid cs-style-1">
+                        <li>
+                            <figure>
+                                <img src="uploads/jobs/ipdec-folder.jpg" title="IPDEC - Impressos / Folder" alt="IPDEC - Impressos / Folder">
+                                <figcaption>
+                                    <h3>Instituto IPDEC</h3>
+                                    <span>Impressos / Folder</span>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </article>
+                <article class="portfolio-section-jobs-article">
+                    <h1 class="fontzero">Trabalhos</h1>
+                    <ul class="grid cs-style-1">
+                        <li>
+                            <figure>
+                                <img src="uploads/jobs/acaimart.jpg" title="Açai Mart - Cartão de Visita e Website" alt="Açai Mart - Cartão de Visita e Website">
+                                <figcaption>
+                                    <h3>Açai Mart</h3>
+                                    <span>Cartão de Visita / Website</span>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </article>
+                <article class="portfolio-section-jobs-article">
+                    <h1 class="fontzero">Trabalhos</h1>
+                    <ul class="grid cs-style-1">
+                        <li>
+                            <figure>
+                                <img src="uploads/jobs/belforthealthcare.jpg" title="Belfort Healthcare - Website" alt="Belfort Healthcare - Website">
+                                <figcaption>
+                                    <h3>Belfort Healthcare</h3>
+                                    <span>Website</span>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </article>
+                <article class="portfolio-section-jobs-article">
+                    <h1 class="fontzero">Trabalhos</h1>
+                    <ul class="grid cs-style-1">
+                        <li>
+                            <figure>
+                                <img src="uploads/jobs/manahh.jpg" title="Manahh for life - Website" alt="Manahh for life - Website">
+                                <figcaption>
+                                    <h3>Manahh for Life</h3>
+                                    <span>Website</span>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </article>
+                <article class="portfolio-section-jobs-article">
+                    <h1 class="fontzero">Trabalhos</h1>
+                    <ul class="grid cs-style-1">
+                        <li>
+                            <figure>
+                                <img src="uploads/jobs/systemax.jpg" title="Systemax - Website" alt="Systemax - Website">
+                                <figcaption>
+                                    <h3>Systemax</h3>
+                                    <span>Website</span>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </article>
+                <article class="portfolio-section-jobs-article">
+                    <h1 class="fontzero">Trabalhos</h1>
+                    <ul class="grid cs-style-1">
+                        <li>
+                            <figure>
+                                <img src="uploads/jobs/amazoncad.jpg" title="Amazoncad - Website" alt="Amazoncad - Website">
+                                <figcaption>
+                                    <h3>Amazoncad</h3>
+                                    <span>Website</span>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </article>
+                    
+                    <?php for ($job = 0; $job <= 8; $job++) { ?>
+
+                        <!-- <article class="portfolio-section-jobs-article">
+                            <h1 class="fontzero">Trabalhos</h1>
+                            <ul class="grid cs-style-1">
+                                <li>
+                                    <figure>
+                                        <img src="uploads/jobs/job01.jpg" title="Jobs" alt="img01">
+                                        <figcaption>
+                                            <h3>Instituto IPDEC</h3>
+                                            <span>Impressos / Folder</span>
+                                        </figcaption>
+                                    </figure>
+                                </li>
+                            </ul>
+                        </article> -->
+
+                    <?php } ?>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="contact-section">
+            <div class="content">
+                <header class="contact-section-title">
+                    <h1>Vamos começar seu <br> novo projeto hoje?</h1>
+                    <p>Entre em contato conosco e solicite <br> um orçamento sem compromisso.</p>
+                    
+                </header>
+                <article class="contact-section-card">
+                    <h2 class="fontzero">Telefone e E-mail</h2>
+                    <!-- <span class="btn btn-big btn-purple radius transition"><i class="fas fa-mobile-alt fa-lg" style="margin-right:15px"></i>92 99341-4404</span> -->
+                    <a class="btn btn-big btn-purple radius transition" href="mailto:contato@belfortweb.com.br" title="Entre em contato!"><i class="far fa-envelope fa-lg" style="margin-right:15px"></i>contato@belfortweb.com.br</a>
+                </article>
+
+                <!-- contato mobile -->
+                <article class="contact-mobile">
+                    <h2 class="fontzero">Telefone e E-mail</h2>
+                    <!-- <div class="phone">
+                        <span><i class="fas fa-phone-square fa-4x"></i></span>
+                        <p>92 99341-4404</p>
+                    </div> -->
+                    <div class="email">
+                        <span><i class="far fa-envelope fa-4x"></i></span>
+                        <p>contato@belfortweb.com.br</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </main>
+
+    <footer class="main-footer">
+        <div class="content">
+            <p>2018 Belfort Web. Todos os Direitos Reservados</p>
+        </div>
+    </footer>
+    
+    <script src="themes/belfortweb/js/toucheffects.js"></script>
+</body>
+</html>
